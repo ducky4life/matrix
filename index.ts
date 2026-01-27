02aa9e892feba5752b694941b68ce5594fae8203
Ducky <124077142+ducky4life@users.noreply.github.com>
2026-01-27 20:46:01 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
+class Matrix {
+    a: number;
+    b: number;
+    c: number;
+    d: number;
 
+    constructor(a=0,b=0,c=0,d=0) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    display(): Array<number> {
+        return([a,b,c,d]);
+    }
+}

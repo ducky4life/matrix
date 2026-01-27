e43e535da238c8259ae9b3f00e7ac57899fbe7c9
Ducky <124077142+ducky4life@users.noreply.github.com>
2026-01-27 20:49:44 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -14,4 +14,16 @@ class Matrix {
     display(): Array<number> {
         return([a,b,c,d]);
     }
+
+    add(M: Matrix): Matrix {
+        return Matrix(a+M.a, b+M.b, c+M.c, d+M.d);
+    }
+
+    minus(M: Matrix): Matrix {
+        return Matrix(a-M.a, b-M.b, c-M.c, d-M.d);
+    }
+
+    multiply(M: Matrix): Matrix {
+        return Matrix(a*M.a+b*M.c, a*M.b+b*M.d, c*M.a+d*M.c, c*M.b+d*M.d);
+    }
 }

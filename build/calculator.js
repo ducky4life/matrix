db19c09bf5104afc8b2338483a97ffe88ac63ff4
Ducky <124077142+ducky4life@users.noreply.github.com>
2026-01-31 18:53:20 +0800

    prepare for 3x3

--- build/calculator.js
+++ build/calculator.js
@@ -1,4 +1,4 @@
-import { Matrix2, getRandomMatrix2 } from "./matrix.js";
+import { Matrix2, Matrix3, getRandomMatrix2 } from "./matrix.js";
 function getInputMatrix2(name) {
     const a1 = document.getElementById(`2x2_${name}_a1`).value;
     const a2 = document.getElementById(`2x2_${name}_a2`).value;
@@ -6,11 +6,34 @@ function getInputMatrix2(name) {
     const b2 = document.getElementById(`2x2_${name}_b2`).value;
     return new Matrix2(Number(a1), Number(a2), Number(b1), Number(b2));
 }
+function getInputMatrix3(name) {
+    const a1 = document.getElementById(`3x3_${name}_a1`).value;
+    const a2 = document.getElementById(`3x3_${name}_a2`).value;
+    const a3 = document.getElementById(`3x3_${name}_a3`).value;
+    const b1 = document.getElementById(`3x3_${name}_b1`).value;
+    const b2 = document.getElementById(`3x3_${name}_b2`).value;
+    const b3 = document.getElementById(`3x3_${name}_b3`).value;
+    const c1 = document.getElementById(`3x3_${name}_c1`).value;
+    const c2 = document.getElementById(`3x3_${name}_c2`).value;
+    const c3 = document.getElementById(`3x3_${name}_c3`).value;
+    return new Matrix3(Number(a1), Number(a2), Number(a3), Number(b1), Number(b2), Number(b3), Number(c1), Number(c2), Number(c3));
+}
 function setInputMatrix2(name, a1, a2, b1, b2) {
     document.getElementById(`2x2_${name}_a1`).value = String(a1);
     document.getElementById(`2x2_${name}_a2`).value = String(a2);
     document.getElementById(`2x2_${name}_b1`).value = String(b1);
     document.getElementById(`2x2_${name}_b2`).value = String(b2);
+}
+function setInputMatrix3(name, a1, a2, a3, b1, b2, b3, c1, c2, c3) {
+    document.getElementById(`3x3_${name}_a1`).value = String(a1);
+    document.getElementById(`3x3_${name}_a2`).value = String(a2);
+    document.getElementById(`3x3_${name}_a3`).value = String(a3);
+    document.getElementById(`3x3_${name}_b1`).value = String(b1);
+    document.getElementById(`3x3_${name}_b2`).value = String(b2);
+    document.getElementById(`3x3_${name}_b3`).value = String(b3);
+    document.getElementById(`3x3_${name}_c1`).value = String(c1);
+    document.getElementById(`3x3_${name}_c2`).value = String(c2);
+    document.getElementById(`3x3_${name}_c3`).value = String(c3);
 }
 function randomiseInput() {
     const M1 = getRandomMatrix2(10);
@@ -91,7 +114,10 @@ function displayOutput(matrix_dimension = 2) {
 document.querySelector('#randomise').addEventListener('click', () => randomiseInput());
 document.querySelector('#clear').addEventListener('click', () => clearInput());
 // (document.querySelector('#submit')as HTMLButtonElement)!.addEventListener('click', () => displayOutput(2));
-const inputElementIds = ['2x2_m1_a1', '2x2_m1_a2', '2x2_m1_b1', '2x2_m1_b2', '2x2_m2_a1', '2x2_m2_a2', '2x2_m2_b1', '2x2_m2_b2', 'm1_property', 'm2_property', 'operation'];
+const inputElementIds = ['2x2_m1_a1', '2x2_m1_a2', '2x2_m1_b1', '2x2_m1_b2', '2x2_m2_a1', '2x2_m2_a2', '2x2_m2_b1', '2x2_m2_b2',
+    '3x3_m1_a1', '3x3_m1_a2', '3x3_m1_a3', '3x3_m1_b1', '3x3_m1_b2', '3x3_m1_b3', '3x3_m1_c1', '3x3_m1_c2', '3x3_m1_c3',
+    'm1_property', 'm2_property', 'operation'
+];
 inputElementIds.forEach((id) => {
     const element = document.getElementById(id);
     element.addEventListener('input', () => displayOutput(2));
--- calculator.ts
+++ calculator.ts
@@ -9,11 +9,37 @@ function getInputMatrix2(name: string) {
     return new Matrix2(Number(a1), Number(a2), Number(b1), Number(b2));
 }
 
+function getInputMatrix3(name: string) {
+    const a1 = (document.getElementById(`3x3_${name}_a1`) as HTMLInputElement).value;
+    const a2 = (document.getElementById(`3x3_${name}_a2`) as HTMLInputElement).value;
+    const a3 = (document.getElementById(`3x3_${name}_a3`) as HTMLInputElement).value;
+    const b1 = (document.getElementById(`3x3_${name}_b1`) as HTMLInputElement).value;
+    const b2 = (document.getElementById(`3x3_${name}_b2`) as HTMLInputElement).value;
+    const b3 = (document.getElementById(`3x3_${name}_b3`) as HTMLInputElement).value;
+    const c1 = (document.getElementById(`3x3_${name}_c1`) as HTMLInputElement).value;
+    const c2 = (document.getElementById(`3x3_${name}_c2`) as HTMLInputElement).value;
+    const c3 = (document.getElementById(`3x3_${name}_c3`) as HTMLInputElement).value;
+
+    return new Matrix3(Number(a1), Number(a2), Number(a3), Number(b1), Number(b2), Number(b3), Number(c1), Number(c2), Number(c3));
+}
+
 function setInputMatrix2(name: string, a1: number, a2: number, b1: number, b2: number) {
     (document.getElementById(`2x2_${name}_a1`) as HTMLInputElement).value = String(a1);
     (document.getElementById(`2x2_${name}_a2`) as HTMLInputElement).value = String(a2);
     (document.getElementById(`2x2_${name}_b1`) as HTMLInputElement).value = String(b1);
     (document.getElementById(`2x2_${name}_b2`) as HTMLInputElement).value = String(b2);
+}
+
+function setInputMatrix3(name: string, a1: number, a2: number, a3: number, b1: number, b2: number, b3: number, c1: number, c2: number, c3: number) {
+    (document.getElementById(`3x3_${name}_a1`) as HTMLInputElement).value = String(a1);
+    (document.getElementById(`3x3_${name}_a2`) as HTMLInputElement).value = String(a2);
+    (document.getElementById(`3x3_${name}_a3`) as HTMLInputElement).value = String(a3);
+    (document.getElementById(`3x3_${name}_b1`) as HTMLInputElement).value = String(b1);
+    (document.getElementById(`3x3_${name}_b2`) as HTMLInputElement).value = String(b2);
+    (document.getElementById(`3x3_${name}_b3`) as HTMLInputElement).value = String(b3);
+    (document.getElementById(`3x3_${name}_c1`) as HTMLInputElement).value = String(c1);
+    (document.getElementById(`3x3_${name}_c2`) as HTMLInputElement).value = String(c2);
+    (document.getElementById(`3x3_${name}_c3`) as HTMLInputElement).value = String(c3);
 }
 
 function randomiseInput() {
@@ -116,7 +142,10 @@ function displayOutput(matrix_dimension: number = 2) {
 (document.querySelector('#clear')as HTMLButtonElement)!.addEventListener('click', () => clearInput());
 // (document.querySelector('#submit')as HTMLButtonElement)!.addEventListener('click', () => displayOutput(2));
 
-const inputElementIds = ['2x2_m1_a1', '2x2_m1_a2', '2x2_m1_b1', '2x2_m1_b2', '2x2_m2_a1', '2x2_m2_a2', '2x2_m2_b1', '2x2_m2_b2', 'm1_property', 'm2_property', 'operation'];
+const inputElementIds = ['2x2_m1_a1', '2x2_m1_a2', '2x2_m1_b1', '2x2_m1_b2', '2x2_m2_a1', '2x2_m2_a2', '2x2_m2_b1', '2x2_m2_b2',
+    '3x3_m1_a1', '3x3_m1_a2', '3x3_m1_a3', '3x3_m1_b1', '3x3_m1_b2', '3x3_m1_b3', '3x3_m1_c1', '3x3_m1_c2', '3x3_m1_c3',
+    'm1_property', 'm2_property', 'operation'
+];
 
 inputElementIds.forEach((id) => {
     const element = (document.getElementById(id) as HTMLInputElement);

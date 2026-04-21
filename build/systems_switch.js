31a20bce843d46aed8730a61e67bce6acee3eb69
Ducky <124077142+ducky4life@users.noreply.github.com>
2026-04-21 12:33:10 +0800

    Update systems_switch.js

--- build/systems_switch.js
+++ build/systems_switch.js
@@ -3,6 +3,7 @@ const toggle = document.getElementById('toggle_mode');
 let currMode = 'calculator';
 function toggleMode() {
     if (currMode == 'calculator') {
+        currMode = 'game';
         const generateButton = document.getElementById('generate');
         const randomiseButton = document.getElementById('randomise');
         const submitButton = document.getElementById('submit');
@@ -11,7 +12,9 @@ function toggleMode() {
         randomiseButton.classList.add('gone');
     }
     else if (currMode == 'game') {
+        currMode = 'calculator';
         setupCalculator();
     }
 }
 toggle.addEventListener('click', () => toggleMode());
+setupCalculator();

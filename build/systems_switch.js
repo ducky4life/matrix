e5a52b13799b5bebe607c34fd2bfb54fda718acf
Ducky <124077142+ducky4life@users.noreply.github.com>
2026-04-21 12:27:25 +0800

    Create systems_switch.js

---
+++ build/systems_switch.js
@@ -0,0 +1,17 @@
+import { setupCalculator } from "./systems_calculator.js";
+const toggle = document.getElementById('toggle_mode');
+let currMode = 'calculator';
+function toggleMode() {
+    if (currMode == 'calculator') {
+        const generateButton = document.getElementById('generate');
+        const randomiseButton = document.getElementById('randomise');
+        const submitButton = document.getElementById('submit');
+        generateButton.classList.remove('gone');
+        submitButton.classList.remove('gone');
+        randomiseButton.classList.add('gone');
+    }
+    else if (currMode == 'game') {
+        setupCalculator();
+    }
+}
+toggle.addEventListener('click', () => toggleMode());
